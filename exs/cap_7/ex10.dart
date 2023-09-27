ffdfab7582e37c306f3e77faf5ead3811b7a18c0
Gabriel Molina <gabriell.2019@alunos.utfpr.edu.br>
2023-09-27 01:49:19 -0300

    ex cap7

--- exs/cap_7/ex10.dart
+++ exs/cap_7/ex10.dart
@@ -5,14 +5,28 @@ void main() {
   int soma1 = 0, soma2 = 0, soma3 = 0, soma4 = 0, soma5 = 0;
 
   for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      print("Valor [$i][$j]: ");
+      matriz[i][j] = int.parse(stdin.readLineSync()!);
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      stdout.write(" ${matriz[i][j]} ");
+    }
+    print("");
+  }
+
+  for (int i = 0; i < 5; i++) {
     soma1 += matriz[3][i];
   }
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 5; i++) {
     soma2 += matriz[i][1];
   }
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5; j++) {
       if (i == j) {
         soma3 += matriz[i][j];
@@ -21,13 +35,12 @@ void main() {
   }
 
   for (int i = 0; i < matriz.length; i++) {
-        soma4 += matriz[i][matriz.length - 1 - i];
-      }
-    
-  for (int i = 0; i < 3; i++) {
+    soma4 += matriz[i][matriz.length - 1 - i];
+  }
+
+  for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5; j++) {
-        soma5 += matriz[i][j];
-      }
+      soma5 += matriz[i][j];
     }
   }
 
