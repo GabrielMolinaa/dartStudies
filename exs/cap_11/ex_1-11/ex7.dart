b1468513f4a67771a5b828c87f3e153a3bdc06cf
Gabriel Molina <gabrielmolinadelima@hotmail.com>
2023-10-06 11:50:04 -0300

    fix: coh_piah input

--- exs/cap_11/ex_1-11/ex7.dart
+++ exs/cap_11/ex_1-11/ex7.dart
@@ -3,22 +3,22 @@ import 'dart:io';
 void main() {
   File recebimentos = File("Recebimentos.csv");
   File clientes = File("Clientes.csv");
-  String nome="";
-   List<String> clientesAux = [];
+  String nome = "";
+  List<String> clientesAux = [];
 
   try {
     print("Digite a data de inicio (YYYY-MM-DD): ");
     String input = stdin.readLineSync()!;
     DateTime dataInicio = DateTime.parse(input);
-    
+
     print("Digite a data limite (YYYY-MM-DD): ");
     input = stdin.readLineSync()!;
     DateTime dataLimite = DateTime.parse(input);
 
     List<String> leitor = recebimentos.readAsLinesSync();
     List<String> leitor2 = clientes.readAsLinesSync();
-    
-    for(int i = 1; i<leitor.length;i++) {
+
+    for (int i = 1; i < leitor.length; i++) {
       String auxiliar = leitor[i].replaceAll(' ', '');
       List<String> recebimentosAux = auxiliar.split(',');
       String codigo = recebimentosAux[4];
@@ -26,21 +26,22 @@ void main() {
       DateTime dataAtual = DateTime.now();
       int diasAtraso = 0;
       if ((data2.isAfter(dataInicio) && data2.isBefore(dataLimite))) {
-          if(data2.isBefore(dataAtual)){
-            Duration atraso = dataAtual.difference(data2);
-            diasAtraso = atraso.inDays;
+        if (data2.isBefore(dataAtual)) {
+          Duration atraso = dataAtual.difference(data2);
+          diasAtraso = atraso.inDays;
+        }
+        leitor2.forEach((element) {
+          clientesAux = element.split(',');
+          if (clientesAux[0] == codigo) {
+            nome = clientesAux[1];
           }
-          leitor2.forEach((element) {
-            clientesAux = element.split(',');
-            if(clientesAux[0] == codigo){
-              nome = clientesAux[1];
-            }
-          });
-      
-          stdout.write("Nome Cliente: $nome || ${leitor[i]} || Dias em Atraso: $diasAtraso\n");
-      }
+        });
 
-    };
+        stdout.write(
+            "Nome Cliente: $nome || ${leitor[i]} || Dias em Atraso: $diasAtraso\n");
+      }
+    }
+    ;
   } catch (e) {
     print("Erro $e");
   }
